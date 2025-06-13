6e37cd9b8d4a89d7e42fe4c26b6a918d4b581548
Zoe Nguyen <nganguyen0703@gmail.com>
2025-06-13 18:47:01 +0800

    chore: initialize basic structure for main.jsx and App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,19 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
+import SiteHeader from "./components/site-header/SiteHeader";
 
+function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Hello</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <SiteHeader />
+      <Routes>
+        {/* <Route path="/" element={<HomePage />} /> */}
+        {/* <Route path="/login" element={<LoginPage />} /> */}
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/site-header/SiteHeader.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SiteHeader() {
+  return (
+    <div>
+      <h1>SiteHeader</h1>
+    </div>
+  )
+};
+
+export default SiteHeader;

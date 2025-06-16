ab11b1bb289bacc4c7e4905feb31d3626e3908a7
Zoe Nguyen <nganguyen0703@gmail.com>
2025-06-16 12:02:29 +0800

    feat: add ClassName helper utils

---
+++ src/components/CustomButton/CustomButton.jsx
@@ -0,0 +1,17 @@
+const Button = ({
+  title,
+  icon,
+  handlePress,
+  containerStyles,
+  type="primary",
+  size="md",
+  state="default",
+}) => {
+  return (
+    <div>
+      
+    </div>
+  )
+};
+
+export default Button;

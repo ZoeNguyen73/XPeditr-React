484a91ebdf2d7037186bdc98399626b201ea95d6
Zoe Nguyen <nganguyen0703@gmail.com>
2025-06-16 11:25:49 +0800

    feat: add useaxiosPrivate hook

---
+++ src/hooks/useAxiosPrivate.js
@@ -0,0 +1,58 @@
+import { useEffect } from "react";
+
+import { axiosPrivate } from "../api/axios";
+import { useAuthContext } from "../context/AuthProvider";
+import useRefreshToken from "./useRefreshToken";
+import handleGlobalError from "../utils/GlobalErrorHandler";
+
+const useAxiosPrivate = () => {
+  const refresh = useRefreshToken();
+  const { auth } = useAuthContext();
+
+  useEffect(() => {
+
+    const requestIntercept = axiosPrivate.interceptors.request.use(
+      config => {
+        if (!config.headers["Authorization"]) {
+          config.headers["Authorization"] = `Bearer ${auth?.accessToken}`;
+        }
+        return config;
+      }, (error) => Promise.reject(error)
+    );
+
+    const responseIntercept = axiosPrivate.interceptors.response.use(
+      response => response,
+      async (error) => {
+        const prevRequest = error?.config;
+
+        // if the 1st request returns 401 Unauthorized and it hasn't been re-requested yet
+        if (error?.response?.status === 401 && !prevRequest?._retry) {
+          prevRequest._retry = true;
+
+          try {
+            const newAccessToken = await refresh();
+            prevRequest.headers["Authorization"] = `Bearer ${newAccessToken}`;
+            return axiosPrivate(prevRequest); // retry the request
+
+          } catch (refreshError) {
+            handleGlobalError(refreshError);
+            return Promise.reject(refreshError); // Reject the original error where refresh fails
+          }
+        }
+
+        return Promise.reject(error); // fallback reject in case the re-request fails
+      }
+        );
+
+    return () => {
+      axiosPrivate.interceptors.request.eject(requestIntercept);
+      axiosPrivate.interceptors.response.eject(responseIntercept);
+    }
+
+  }, [auth, refresh]);
+
+  return axiosPrivate;
+
+};
+
+export default useAxiosPrivate;
